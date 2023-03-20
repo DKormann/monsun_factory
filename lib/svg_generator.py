5b6c098ee1c9a8d42119c3f4dc4e8e348a7de296
Dominik Kormann <dominikormann@outlook.com>
2023-03-20 18:43:19 +0100

    bow gen

--- lib/svg_generator.py
+++ lib/svg_generator.py
@@ -1,26 +1,26 @@
 import math
 svg_template = '''
 <div >
-<svg height="40vh%" viewBox="0 0 1000 1000" width="40vh" style="background:white;fill-rule:nonzero;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;"  >
+<svg width="{}" viewBox="0 0 {} {}"  style="background:{};fill-rule:nonzero;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;"  >
 {}
 </svg>
 </div>
 '''
-def svg(*items): return svg_template.format("\n".join(items))
+def svg(*items,outer_width= "40vh",width=1000,height =1000,background_color='white'): return svg_template.format(outer_width,width,height,background_color,"\n".join(items))
 
-path_template = '''<path d="{}" fill="none" opacity="1" stroke="{}" stroke-linecap="round" stroke-linejoin="round" stroke-width="7"/>'''
+path_template = '''<path d="{}" fill="{}" opacity="1" stroke="{}" stroke-linecap="round" stroke-linejoin="round" stroke-width="{}"/>'''
 fill_template = '''<path d="{}" fill="{}" opacity="1" stroke="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="7"/>'''
 
-def line_from_path(content,color = '#000'):return path_template.format(content,color)
+def line_from_path(content,color = '#000',fill = "none",stroke_width = 7):return path_template.format(content,fill,color,stroke_width)
 def fill_from_path(content,color = '#000'):return fill_template.format(content,color)
 
 def points_to_path(*points:tuple[float,float]):
-    points = map(lambda p:str(p[0])+' '+str(p[1]),points)
+    points = map(lambda p:str(round(p[0],3))+' '+str(round(p[1],3)),points)
     path = "M"+"L".join(points)
     return path
 
-def line(*points,color='#000'):
-    return line_from_path(points_to_path(*points),color)
+def line(*points,color='#000',fill= 'none',stroke_width='7'):
+    return line_from_path(points_to_path(*points),color,fill,stroke_width)
 def fill(*points,color='#000'):
     return fill_from_path(points_to_path(*points),color)
 
@@ -108,3 +108,78 @@ def diagram(nums, names, ylabel = ""):
 
     html = f'''<div style="background:white;display:inline">  {vec}</div>'''
     return html
+
+text = lambda x,y,txt : f'<text x="{x}" y="{y}" font-size="35">{txt}</text>'
+
+view_size = 1000
+min_pad = 7
+
+light_yellow = '#f0ee99'
+
+"""generate a rectangular vector image grid"""
+class Grid:
+    def __init__(self,X,Y,width = "40vh"):
+        
+        self.width = width
+
+            
+        self.X,self.Y = X,Y
+
+        #available figure size
+        self.fig_size = view_size - min_pad*2
+
+        #max side cellcount 
+        W = max(X,Y)
+
+        #cell size
+        self.c = self.fig_size/W
+
+        self.w = X*self.c
+        self.h = Y*self.c
+
+        self.lines = [
+            *[line(self.rel(0,i),self.rel(X,i),color=light_yellow) for i in range(Y+1)],
+            *[line(self.rel(i,0),self.rel(i,Y),color=light_yellow) for i in range(X+1)],
+        ]
+
+    def relx(self,x):return x*self.c+min_pad
+    def rely(self,y):return y*self.c+min_pad
+    def rel(self,x,y):return (self.relx(x),self.rely(y))
+
+    def add_figure(self,*points,color="#000",fill= "#fff"):
+        self.lines.append(line(
+            *[self.rel(*point)for point in points],
+            color=color,fill = fill,
+        ))
+
+    def get_html(self):
+        html = svg(*self.lines,outer_width=self.width,width=self.w+min_pad*2, height= self.h+min_pad*2)
+        return html
+
+
+deg2rad = lambda deg: deg/180*math.pi
+def rad2cart(rad:float):
+    y = 1-math.cos(rad)
+    x = math.sin(rad)
+    return x,y
+def bow_point(center, r, rad):
+    y = (-math.cos(rad))*r
+    x = math.sin(rad)*r
+    return (center[0]+x,center[1]+y)
+
+def bow(center,r:float,start:float,end:float):
+    if end<start:
+        end += 360
+    
+    start = deg2rad(start)
+    end = deg2rad(end)
+
+    p = []
+
+    while start < end:
+        p.append(bow_point(center,r,start))
+        start += 0.2
+    
+    p.append(bow_point(center,r,end))
+
+    return p
